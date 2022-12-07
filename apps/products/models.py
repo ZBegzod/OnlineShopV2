89543ddd66ca37b14c76e3f73a28e37576fd2879
Zohidoff <zohidov.begzod@mail.ru>
2022-12-07 19:37:10 +0300

    *

--- apps/products/models.py
+++ apps/products/models.py
@@ -186,37 +186,3 @@ class Order(models.Model):
 
     def __str__(self):
         return f"{self.cart.customer}"
-
-
-class OrderItem(models.Model):
-
-    order = models.ForeignKey(
-        Order,
-        related_name='order_items',
-        on_delete=models.CASCADE
-    )
-
-    product = models.ForeignKey(
-        Product,
-        related_name='order_items',
-        on_delete=models.CASCADE
-    )
-
-    STATUS = [
-
-        ('paid', 'Paid'),
-        ('canceled', 'Canceled'),
-        ('waiting_for_payment', 'Waiting_for_payment'),
-
-    ]
-
-    status = models.CharField(max_length=120, choices=STATUS)
-    quantity = models.PositiveIntegerField(null=True, blank=True)
-
-    @property
-    def get_total_price(self):
-        return f"{self.quantity * self.product.price}"
-
-    def __str__(self):
-        return f"{self.product.name}"
-
